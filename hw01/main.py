223f3ccabb832759fc3bab7e01fb362e46141b92
etrudx3 <nopparit.intharasombat@mckesson.com>
2019-03-04 23:59:45 +0000

    hw # 1

---
+++ hw01/main.py
@@ -0,0 +1,40 @@
+"""
+Pirple Python HW # 1
+Description:
+This exercise is for variable creation and assignment by
+creating collection of attributes describing the song.
+
+3/4/2019
+"""
+
+# Song title (String)
+Title = 'Havana'
+# Length of song in seconds (Float)
+DurationInSeconds = 217.391
+# Name of artist (String)
+Artist = 'Camila Cabello'
+# Album name (String)
+Album = 'Camila'
+# Song's genre (String)
+Genre = 'Pop'
+# Year released (Integer)
+Year = 2017
+# Release date (String)
+ReleasedDate = 'September 8, 2017'
+# Track number in the album (Integer)
+Track = 4
+# Recording Label (string)
+Label = 'Epic'
+# Song's producer(s) (String)
+Producers = 'Frank Dukes, Matt Beckley'
+
+print(Title)
+print(DurationInSeconds)
+print(Artist)
+print(Album)
+print(Genre)
+print(Year)
+print(ReleasedDate)
+print(Track)
+print(Label)
+print(Producers)
